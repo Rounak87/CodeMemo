3f11c4ab5e73fa788784c1bf8b85ec8167bcb0f1
Rounak singh <rounaksingh0807@gmail.com>
2025-02-09 20:13:43 +0530

    home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,44 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useSearchParams } from "react-router-dom";
 
 const Home = () => {
+  const [Title, setTitle] = useState("");
+  const [Memos, setMemos] = useState("");
+  const [searchParams, setsearchParams] = useSearchParams();
+  const memoId = searchParams.get("memoId");
+
   return (
-    <div className='flex justify-center mt-5'>
     <div>
-        <input type="text" 
-        placeholder='Enter Title'
-        className='border outline-none border-gray-400 py-2 pl-4 w-[200px] sm:w-[400px] mt-2 rounded-2xl'
-        
+      <div className="flex justify-center mt-5 gap-4">
+        <div>
+          <input
+            type="text"
+            placeholder="Title"
+            value={Title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="border outline-none border-gray-400 py-3 pl-4 w-[200px] sm:w-[700px] mt-2 rounded-2xl bg-gray-800 text-white focus:outline-none "
+          />
+        </div>
+        <button className=" py-3 px-1 sm:py-3 sm:px-5  m-2  rounded-xl cursor-pointer border border-gray-600  shadow-lg transition-all duration-200 bg-gray-700 sm:hover:bg-gray-800 hover:bg-gray-800 ">
+          {memoId ? "Update Memo" : "Add Memo"}
+        </button>
+      </div>
+
+      <div className=" flex justify-center mt-5">
+        <div className="relative">
+        <button className="  absolute z-10 cursor-pointer mt-4 right-4" >copy</button>
+        <textarea
+          value={Memos}
+          onChange={(e) => setMemos(e.target.value)}
+          placeholder="Enter your code here"
+          rows={22}
+          className="  border outline-none border-gray-400 py-3 pl-4 w-[350px] sm:w-[860px] mt-2 rounded-l bg-gray-800 text-white focus:outline-none "
         />
+        </div>
+      </div>
 
     </div>
-    <button className='py-2 px-1 m-2  rounded-xl cursor-pointer border border-gray-600 lg:px-3 shadow-lg transition-all duration-200 bg-gray-700 hover:bg-gray-800' >Add To Memos</button>
-    </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-center gap-10  text-2xl bg-gray-900 text-white py-3 rounded-lg shadow-lg">
+    <div className="flex justify-center gap-5 text-2xl bg-gray-900 text-white py-2 rounded-lg shadow-lg">
       <NavLink to="/"
        className={({isActive})=>`px-4 py-2 rounded-lg hover:text-amber-300 transition-colors
        ${isActive ? 'text-amber-300 font-semibold' : ''}
